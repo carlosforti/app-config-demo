5e0d4eee9824dd9a1ec87d1373619e9260e38b9a
Carlos Eduardo Cassaniga Forti <carlosforti@carlosforti.com.br>
2020-03-11 10:47:40 -0300

    Adicionando conexão com banco de dados

---
+++ Database/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using app_config_demo.Models;
+
+namespace app_config_demo.Database
+{
+    public class UserRepository
+    {
+        public IEnumerable<User> GetAll()
+        {
+            var query = new StringBuilder();
+            query.AppendLine("SELECT FirstName,");
+            query.AppendLine("       LastName,");
+            query.AppendLine("       Email,");
+            query.AppendLine("       Gender,");
+            query.AppendLine("       IpAddress,");
+            query.AppendLine("       Country");
+            query.AppendLine("FROM   User");
+
+            return new List<User>();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_config_demo.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string IpAddress { get; set; }
+        public string Country { get; set; }
+    }
+}
